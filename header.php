c69f6893b7c9512855e900652747e04e066eb554
Greg Priday <greg@siteorigin.com>
2013-08-19 23:19:24 +0000

    Running updates to Vantage theme.

--- header.php
+++ header.php
@@ -22,34 +22,15 @@
 
 <body <?php body_class(); ?>>
 
+<?php do_action('vantage_before_page_wrapper') ?>
+
 <div id="page-wrapper">
-	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup class="full-container">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><?php vantage_display_logo(); ?></a>
 
-			<div class="support-text">
-				<?php do_action('vantage_support_text'); ?>
-			</div>
-		</hgroup>
+	<?php do_action( 'vantage_before_masthead' ); ?>
 
-		<nav role="navigation" class="site-navigation main-navigation primary <?php if(siteorigin_setting('general_use_sticky_menu')) echo 'use-sticky-menu' ?>">
-			<div class="full-container">
-				<h1 class="assistive-text"><?php _e( 'Menu', 'vantage' ); ?></h1>
-				<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'vantage' ); ?>"><?php _e( 'Skip to content', 'vantage' ); ?></a></div>
+	<?php get_template_part( 'parts/masthead', apply_filters( 'vantage_masthead_type', '' ) ); ?>
 
-				<?php if(siteorigin_setting('general_menu_search')) : ?>
-					<div id="search-icon">
-						<form method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-							<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" />
-						</form>
-					</div>
-				<?php endif; ?>
-
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'link_before' => '<span class="icon"></span>' ) ); ?>
-			</div>
-		</nav><!-- .site-navigation .main-navigation -->
-	</header><!-- #masthead .site-header -->
+	<?php do_action( 'vantage_after_masthead' ); ?>
 
 	<div id="slider">
 		<div class="full-container">
