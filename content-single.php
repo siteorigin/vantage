072993e03532874c42b9cce449095b930331ae39
Greg Priday <greg@siteorigin.com>
2013-08-21 10:59:51 +0000

    Running updates to Vantage theme.

--- content-single.php
+++ content-single.php
@@ -8,21 +8,26 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
 
 	<div class="entry-main">
+
+		<?php do_action('vantage_before_single_entry') ?>
+
 		<header class="entry-header">
+
 			<?php if( has_post_thumbnail() ): ?>
 				<div class="entry-thumbnail"><?php the_post_thumbnail() ?></div>
 			<?php endif; ?>
 
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'vantage' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
-			<?php if ( 'post' == get_post_type() ) : ?>
+			<?php if ( get_post_type() == 'post' ) : ?>
 				<div class="entry-meta">
 					<?php vantage_posted_on(); ?>
 				</div><!-- .entry-meta -->
 			<?php endif; ?>
+
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
@@ -32,6 +37,8 @@
 				<?php the_tags('<p class="tags"><strong>'.__('Tags: ', 'vantage').'</strong>', ', ', '</p>') ?>
 			</p>
 		</div><!-- .entry-content -->
+
+		<?php do_action('vantage_after_single_entry') ?>
 
 	</div>
 
