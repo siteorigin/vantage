66f583153bce4a0914672db4d2140d90f68e9c00
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-08-30 12:17:01 +0000

    Running updates to Vantage theme.

--- themes/vantage/trunk/parts/footer.php
+++ themes/vantage/trunk/parts/footer.php
@@ -3,9 +3,9 @@
 	<div id="footer-widgets" class="full-container">
 		<?php dynamic_sidebar( 'sidebar-footer' ) ?>
 	</div><!-- #footer-widgets -->
+
 	<div class="site-info">
-		<?php do_action( 'vantage_credits' ); ?>
-		<?php echo apply_filters( 'vantage_credits_siteorigin', sprintf( __( 'Designed by %1$s.', 'vantage' ), '<a href="http://siteorigin.com/" rel="designer">SiteOrigin</a>' ) ); ?>
+		<?php do_action('vantage_site_info') ?>
 	</div><!-- .site-info -->
 
 	<?php echo apply_filters( 'vantage_footer_attribution', '<div id="theme-attribution">' . sprintf( __('A <a href="%s">SiteOrigin</a> Theme'), 'http://siteorigin.com') . '</div>' ) ?>
