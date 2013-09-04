7154e057f2ba78f0e3722ae61b47e90c791ce1cd
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-09-04 21:46:05 +0000

    Running updates to vantage

--- themes/vantage/trunk/slider/demo.php
+++ themes/vantage/trunk/slider/demo.php
@@ -1,9 +1,29 @@
-<?php wp_enqueue_script('vantage-demo-slider', get_template_directory().'/slider/demo.js', array('jquery'), SITEORIGIN_THEME_VERSION) ?>
+<?php
+wp_enqueue_script('vantage-demo-slider', get_template_directory_uri().'/slider/demo.js', array('jquery'), SITEORIGIN_THEME_VERSION);
+wp_enqueue_style('vantage-demo-slider', get_template_directory_uri().'/slider/demo.css', array(), SITEORIGIN_THEME_VERSION);
+wp_enqueue_style('vantage-demo-slider-animate', get_template_directory_uri().'/slider/animate.css', array(), SITEORIGIN_THEME_VERSION);
+?>
 
-<div class="flexslider" id="metaslider_demo">
+<div class="flexslider" id="metaslider-demo">
 	<ul class="slides">
-		<li class="slide">
 
+		<li>
+			<div class="content">
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/city.jpg" width="1080" height="420" />
+			</div>
 		</li>
+
+		<li>
+			<div class="content">
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/space.jpg" width="1080" height="420" />
+			</div>
+		</li>
+
+		<li>
+			<div class="content">
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/leaves.jpg" width="1080" height="420" />
+			</div>
+		</li>
+
 	</ul>
 </div>
