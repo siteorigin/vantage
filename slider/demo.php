f99f79324a4e70b14431897f70f3b29cbe2a5fac
Greg Priday <greg@siteorigin.com>
2013-09-06 09:37:03 +0000

    Improved demo slider.

--- slider/demo.php
+++ slider/demo.php
@@ -1,28 +1,19 @@
-<?php
-wp_enqueue_script('vantage-demo-slider', get_template_directory_uri().'/slider/demo.js', array('jquery'), SITEORIGIN_THEME_VERSION);
-wp_enqueue_style('vantage-demo-slider', get_template_directory_uri().'/slider/demo.css', array(), SITEORIGIN_THEME_VERSION);
-wp_enqueue_style('vantage-demo-slider-animate', get_template_directory_uri().'/slider/animate.css', array(), SITEORIGIN_THEME_VERSION);
-?>
-
 <div class="flexslider" id="metaslider-demo">
 	<ul class="slides">
 
 		<li>
 			<div class="content">
-				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-1.jpg" width="1080" height="420" />
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-responsive-overlay.png" width="1080" height="420" />
 			</div>
+
+			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-4.jpg" class="msDefaultImage" width="1080" height="420" />
 		</li>
 
 		<li>
 			<div class="content">
-				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-2.jpg" width="1080" height="420" />
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-plugins-overlay.png" width="1080" height="420" />
 			</div>
-		</li>
-
-		<li>
-			<div class="content">
-				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-3.jpg" width="1080" height="420" />
-			</div>
+			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-1.jpg" class="msDefaultImage" width="1080" height="420" />
 		</li>
 
 	</ul>
