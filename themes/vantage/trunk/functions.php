1074425170b8a425fbdd662ca43a815602ebab20
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-09-06 10:32:29 +0000

    Removed debug stuff from vantage.

--- themes/vantage/trunk/functions.php
+++ themes/vantage/trunk/functions.php
@@ -10,7 +10,7 @@
 define('SITEORIGIN_THEME_VERSION', 'trunk');
 define('SITEORIGIN_THEME_ENDPOINT', 'http://siteorigin.dynalias.com');
 
-if( file_exists( get_template_directory() . '/premium/functions.php' ) && false ){
+if( file_exists( get_template_directory() . '/premium/functions.php' ) ){
 	include get_template_directory() . '/premium/functions.php';
 }
 else {
