b290751aaa84859150425f43ae61f94f4474882a
Greg Priday <greg@siteorigin.com>
2013-09-11 16:04:27 +0000

    Fixed translation domains.

--- fontawesome/icon-sections.php
+++ fontawesome/icon-sections.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-	'web_application_icons' => __('Web Application Icons', 'vanage'),
-	'currency_icons' => __('Currency Icons', 'vanage'),
-	'text_editor_icons' => __('Text Editor Icons', 'vanage'),
-	'directional_icons' => __('Directional Icons', 'vanage'),
-	'video_player_icons' => __('Video Player Icons', 'vanage'),
-	'brand_icons' => __('Brand Icons', 'vanage'),
-	'medical_icons' => __('Medical Icons', 'vanage'),
+	'web_application_icons' => __('Web Application Icons', 'vantage'),
+	'currency_icons' => __('Currency Icons', 'vantage'),
+	'text_editor_icons' => __('Text Editor Icons', 'vantage'),
+	'directional_icons' => __('Directional Icons', 'vantage'),
+	'video_player_icons' => __('Video Player Icons', 'vantage'),
+	'brand_icons' => __('Brand Icons', 'vantage'),
+	'medical_icons' => __('Medical Icons', 'vantage'),
 );
