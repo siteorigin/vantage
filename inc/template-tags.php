80068d7d414cef2af023004ff6a8d91a3dffbcaf
Greg Priday <greg@siteorigin.com>
2013-09-11 15:53:30 +0000

    Fixed pagination to pass theme check.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -332,9 +332,10 @@ function vantage_next_attachment_url($post = null){
  * @param int $range
  */
 function vantage_pagination($pages = '', $range = 2) {
+
 	$showitems = ($range * 2)+1;
 
-	global $wp_query;
+	global $wp_query, $wp_rewrite;
 	$paged = $wp_query->get('paged');
 	if(empty($paged)) $paged = 1;
 
@@ -346,17 +347,13 @@ function vantage_pagination($pages = '', $range = 2) {
 
 	if(1 != $pages) {
 		echo "<div class='pagination'>";
-		if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo;</a>";
-		if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a>";
-
-		for ($i=1; $i <= $pages; $i++) {
-			if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )) {
-				echo ($paged == $i)? "<span class='current'>".$i."</span>":"<a href='".get_pagenum_link($i)."' class='inactive' >".$i."</a>";
-			}
-		}
-
-		if ($paged < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a>";
-		if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>&raquo;</a>";
+		echo paginate_links(array(
+			'total' => $pages,
+			'current' => $paged,
+			'mid_size' => $showitems,
+			'format' => $wp_rewrite->permalink_structure == '' ? ( strpos(get_pagenum_link(false), '?') === false ? '?paged=%#%' : '&paged=%#%' ) : 'page/%#%/',
+			'base' => get_pagenum_link(false).'%_%',
+		));
 		echo "</div>\n";
 	}
 }
