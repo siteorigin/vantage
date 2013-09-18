d1b4f209dc1f3546389c4f6c2bab1c19d5af23e7
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-09-18 12:03:37 +0000

    Removed author bio setting.

--- themes/vantage/trunk/premium/inc/settings.php
+++ themes/vantage/trunk/premium/inc/settings.php
@@ -8,12 +8,9 @@
  */
 function vantage_premium_theme_settings(){
 	// Implement all the teaser settings
-
 	siteorigin_settings_add_field('logo', 'image_retina', 'media');
 
 	siteorigin_settings_add_field('navigation', 'responsive_menu', 'checkbox');
-
-	siteorigin_settings_add_field('blog', 'author_bio', 'checkbox');
 
 	siteorigin_settings_add_field('social', 'ajax_comments', 'checkbox');
 	siteorigin_settings_add_field('social', 'share_post', 'checkbox');
