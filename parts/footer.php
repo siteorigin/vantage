cd46849a3b9c9a50f31ac9b92de426a1244bf039
Greg Priday <greg@siteorigin.com>
2013-09-25 12:54:20 +0000

    Added a setting for footer copyright text.

--- parts/footer.php
+++ parts/footer.php
@@ -4,9 +4,11 @@
 		<?php dynamic_sidebar( 'sidebar-footer' ) ?>
 	</div><!-- #footer-widgets -->
 
-	<div class="site-info">
-		<?php do_action('vantage_site_info') ?>
-	</div><!-- .site-info -->
+	<?php $site_info_text = apply_filters('vantage_site_info', siteorigin_setting('general_site_info_text') ); if( !empty($site_info_text) ) : ?>
+		<div id="site-info">
+			<?php echo wp_kses_post($site_info_text) ?>
+		</div><!-- .site-info -->
+	<?php endif; ?>
 
 	<?php echo apply_filters( 'vantage_footer_attribution', '<div id="theme-attribution">' . sprintf( __('A <a href="%s">SiteOrigin</a> Theme'), 'http://siteorigin.com') . '</div>' ) ?>
 
