20070fd2c201f3cfe449203d9fdeff54d9f0da62
Greg Priday <greg@siteorigin.com>
2013-09-25 09:33:56 +0000

    Improved WooCommerce support.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -6,13 +6,3 @@
  * @since 1.0
  * @license GPL 2.0
  */
-
-function vantage_woocommerce_before_single_product(){
-	?><div id="primary"><?php
-}
-add_action('woocommerce_before_single_product', 'vantage_woocommerce_before_single_product');
-
-function vantage_woocommerce_after_single_product(){
-	?></div><?php
-}
-add_action('woocommerce_after_single_product', 'vantage_woocommerce_after_single_product');
