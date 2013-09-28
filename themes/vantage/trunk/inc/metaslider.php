bc748a031a6390abbdca999e3c7e39bfa0ab6f09
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-09-28 08:37:09 +0000

    Added a way to set images via caption.

--- themes/vantage/trunk/inc/metaslider.php
+++ themes/vantage/trunk/inc/metaslider.php
@@ -5,3 +5,38 @@ function vantage_metaslider_themes($themes, $current){
 	return $themes;
 }
 add_filter('metaslider_get_available_themes', 'vantage_metaslider_themes', 5, 2);
+
+/**
+ * Change the HTML for the home page slider.
+ *
+ * @param $html
+ * @param $slide
+ * @param $settings
+ */
+function vantage_metaslider_filter_flex_slide($html, $slide, $settings){
+	if( is_admin() ) return $html;
+
+	if(!empty($slide['caption']) && function_exists('filter_var') && filter_var($slide['caption'], FILTER_VALIDATE_URL) !== false) {
+
+		$html = sprintf("<img src='%s' class='msDefaultImage' width='%d' width='%d' />", $slide['thumb'], intval($settings['width']), intval($settings['height']));
+
+		if (strlen($slide['url'])) {
+			$html = '<a href="' . $slide['url'] . '" target="' . $slide['target'] . '">' . $html . '</a>';
+		}
+
+		$caption = '<div class="content">';
+		$caption .= sprintf('<img src="%s" width="%d" height="%d" />', esc_url($slide['caption']), intval($settings['width']), intval($settings['height']));
+		$caption .= '</div>';
+
+		$html = $caption . $html;
+
+		$thumb = isset($slide['data-thumb']) && strlen($slide['data-thumb']) ? " data-thumb=\"{$slide['data-thumb']}\"" : "";
+
+		$html = '<li style="display: none;"' . $thumb . ' class="vantage-slide-with-image">' . $html . '</li>';
+	}
+
+	error_log($html);
+
+	return $html;
+}
+add_filter('metaslider_image_flex_slider_markup', 'vantage_metaslider_filter_flex_slide', 10, 3);
