e33f40aa4ced083eb04d5dda80a1ac23408d9271
Greg Priday <greg@siteorigin.com>
2013-10-02 13:51:19 +0000

    Added Page Builder element styles to Premium version.

---
+++ premium/panels-widgets/button/presets/vantage_flat.php
@@ -0,0 +1,46 @@
+<?php
+
+return array(
+	'red' => array(
+		'background_color' => '#db0019',
+	),
+	'berry' => array(
+		'background_color' => '#ea004a',
+	),
+	'orange' => array(
+		'background_color' => '#f65800',
+	),
+	'gold' => array(
+		'background_color' => '#ed9f00',
+	),
+	'pink' => array(
+		'background_color' => '#ff73a9',
+	),
+	'green' => array(
+		'background_color' => '#69c723',
+	),
+	'dark_green' => array(
+		'background_color' => '#00b450',
+	),
+	'light_blue' => array(
+		'background_color' => '#00c5ea',
+	),
+	'blue' => array(
+		'background_color' => '#2a6ce5',
+	),
+	'dark_blue' => array(
+		'background_color' => '#0677c0',
+	),
+	'violet' => array(
+		'background_color' => '#7113b1',
+	),
+	'black' => array(
+		'background_color' => '#393b3b',
+	),
+	'white' => array(
+		'background_color' => '#efefef',
+	),
+	'grey' => array(
+		'background_color' => '#9b9c9d',
+	),
+);
