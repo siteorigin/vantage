246aa4312364c5071edc15397b3f3d6560df1d74
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-10-04 06:31:13 +0000

    Tagged Vantage

---
+++ themes/vantage/tags/1.0.8/inc/woocommerce.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Integrates this theme with WooCommerce.
+ *
+ * @package vantage
+ * @since 1.0
+ * @license GPL 2.0
+ */
---
+++ themes/vantage/tags/1.0.8/loops/loop-slider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Just displays a slider loop. Intended to be included in child themes using get_template_part('loop', 'slider'). Also works with SiteOrigin page builder loop widget.
+ *
+ * Loop Name: Slider
+ *
+ * @package vantage
+ * @since vantage 1.0
+ * @license GPL 2.0
+ */
+?>
+
+<?php if ( have_posts() ) : ?>
+
+<div class="flexslider-wrapper">
+	<div class="flexslider">
+		<ul class="slides">
+			<?php while ( have_posts() ) : the_post(); if( has_post_thumbnail() ) : ?>
+				<li class="slide">
+					<a href="<?php the_permalink() ?>">
+						<?php the_post_thumbnail('vantage-slide'); ?>
+						<div class="flex-caption">
+							<h3><?php the_title() ?></h3>
+						</div>
+					</a>
+				</li>
+			<?php endif; endwhile; ?>
+		</ul>
+	</div>
+</div>
+
+<?php endif; ?>
