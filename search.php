3739004e57f551b27801ec25306433e2ceaee47a
Greg Priday <greg@siteorigin.com>
2013-10-04 04:15:52 +0000

    Fixed archive pages.

--- search.php
+++ search.php
@@ -21,7 +21,7 @@ get_header(); ?>
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'search' ); ?>
+			<?php get_template_part( 'content' ); ?>
 
 		<?php endwhile; ?>
 
