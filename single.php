64fe9dcc0840708c6ad0106093518482ce4aa0fb
Greg Priday <greg@siteorigin.com>
2013-10-08 07:44:55 +0000

    Added setting for next/previous post navigation.

--- single.php
+++ single.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 		<?php get_template_part( 'content', 'single' ); ?>
 
-		<?php vantage_content_nav( 'nav-below' ); ?>
+		<?php if( siteorigin_setting('navigation_post_nav') ) vantage_content_nav( 'nav-below' ); ?>
 
 		<?php if ( comments_open() || '0' != get_comments_number() ) : ?>
 			<?php comments_template( '', true ); ?>
