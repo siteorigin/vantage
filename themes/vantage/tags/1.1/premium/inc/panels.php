191f5e7d096911e21045117d22162ac80f9687bc
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-10-16 17:41:21 +0000

    Tagged Vantage

--- themes/vantage/tags/1.1/premium/inc/panels.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function vantage_premium_add_widget_folders($folders){
-	$folders[get_template_directory().'/premium/panels-widgets/'] = get_template_directory_uri().'/premium/panels-widgets/';
-	return $folders;
-}
-add_action('siteorigin_widget_folders', 'vantage_premium_add_widget_folders');
