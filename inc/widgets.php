42f5a4945ffcbdd2a6be46b78cb524e84f0872ac
Greg Priday <greg@siteorigin.com>
2013-10-17 07:26:12 +0000

    Moved Social widget into free. Premium social widget is slightly enhanced.

--- inc/widgets.php
+++ inc/widgets.php
@@ -214,9 +214,118 @@ class Vantage_Headline_Widget extends WP_Widget {
 }
 
 /**
+ * A widget for display social media networks
+ *
+ * Class Vantage_Social_Media_Widget
+ */
+class Vantage_Social_Media_Widget extends WP_Widget{
+
+	private $networks;
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'vantage-social-media',
+			__('Vantage Social Media', 'vantage'),
+			array(
+				'description' => __( 'Add nice little icons that link out to your social media profiles.', 'text_domain' )
+			)
+		);
+
+		$this->networks = apply_filters('vantage_social_widget_networks', array(
+			'facebook' => __('Facebook', 'vantage'),
+			'twitter' => __('Twitter', 'vantage'),
+			'google-plus' => __('Google Plus', 'vantage'),
+			'rss' => __('RSS', 'vantage'),
+		));
+	}
+
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+		echo $args['before_widget'];
+
+		if(!empty($instance['title'])) {
+			echo $args['before_title'].$instance['title'].$args['after_title'];
+		}
+
+		foreach($this->networks as $id => $name) {
+			if(!empty($instance[$id])) {
+				?><a class="social-media-icon social-media-icon-<?php echo $id ?> social-media-icon-<?php echo esc_attr($instance['size']) ?>" href="<?php echo esc_url( $instance[$id] ) ?>" title="<?php echo esc_html( get_bloginfo('name') . ' ' . $name ) ?>" <?php if(!empty($instance['new_window'])) echo 'target="_blank"'; ?>><?php
+
+				$icon = apply_filters('vantage_social_widget_icon_'.$id, '');
+				if(!empty($icon)) echo $icon;
+				else echo '<span class="icon-' . $id . '" />';
+
+				?></a><?php
+			}
+		}
+
+		echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+		$instance = wp_parse_args($instance, array(
+			'size' => 'medium',
+			'title' => '',
+			'new_window' => false,
+		) );
+
+		$sizes = apply_filters('vantage_social_widget_sizes', array(
+			'medium' => __('Medium', 'vantage'),
+		));
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title') ?>"><?php _e('Title') ?></label><br/>
+			<input type="text" name="<?php echo $this->get_field_name('title') ?>" id="<?php echo $this->get_field_id('title') ?>" value="<?php echo esc_attr($instance['title']) ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('size') ?>"><?php _e('Icon Size') ?></label><br/>
+			<select id="<?php echo $this->get_field_id('size') ?>" name="<?php echo $this->get_field_name('size') ?>">
+				<?php foreach($sizes as $id => $name) : ?>
+					<option value="<?php echo esc_attr($id) ?>" <?php selected($instance['size'], $id) ?>><?php echo esc_html($name) ?></option>
+				<?php endforeach; ?>
+			</select>
+		</p>
+		<?php
+
+		foreach($this->networks as $id => $name) {
+			?>
+			<p>
+				<label for="<?php echo $this->get_field_id($id) ?>"><?php echo $name ?></label>
+				<input type="text" id="<?php echo $this->get_field_id($id) ?>" name="<?php echo $this->get_field_name($id) ?>" value="<?php echo esc_attr(!empty($instance[$id]) ? $instance[$id] : '') ?>" class="widefat"/>
+			</p>
+		<?php
+		}
+
+		?>
+		<p>
+			<input type="checkbox" name="<?php echo $this->get_field_name('new_window') ?>" id="<?php echo $this->get_field_id('new_window') ?>" <?php checked($instance['new_window']) ?> />
+			<label for="<?php echo $this->get_field_id('new_window') ?>"><?php _e('Open in New Window') ?></label>
+
+		</p>
+		<?php
+
+		if(!defined('SITEORIGIN_IS_PREMIUM')) {
+			?>
+			<p><?php printf(__('Additional networks and sizes are available in <a href="%s" target="_blank">Vantage Premium</a>.'), admin_url('themes.php?page=premium_upgrade')) ?></p>
+			<?php
+		}
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$new_instance['new_window'] = !empty($new_instance['new_window']);
+		return $new_instance;
+	}
+}
+
+/**
  * Register the Vantage specific widgets.
  */
 function vantage_register_widgets(){
+	register_widget('Vantage_Social_Media_Widget');
 	register_widget('Vantage_CircleIcon_Widget');
 	register_widget('Vantage_Headline_Widget');
 }
