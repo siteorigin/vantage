c6a306b9b8356088dae4bd12855398abaf0fba16
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-10-17 07:33:04 +0000

    Tagged Vantage

---
+++ themes/vantage/tags/1.1/premium/panels-widgets/call-to-action/presets/simple.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+	'vantage_flat_grey' => array(
+		'borders' => '1px solid #DDDDDD',
+		'background_color' => '#F6F6F6',
+		'rounding' => '3px',
+		'texture' => 'none',
+		'title_text_color' => '#444444',
+		'subtitle_text_color' => '#777777',
+		'subtitle_font_weight' => '400',
+		'shadow' => 0,
+	),
+
+	'vantage_flat_white' => array(
+		'borders' => 'none',
+		'background_color' => '#FCFCFC',
+		'rounding' => '3px',
+		'texture' => 'none',
+		'title_text_color' => '#444444',
+		'subtitle_text_color' => '#777777',
+		'subtitle_font_weight' => '400',
+		'shadow' => 0,
+	),
+
+	'vantage_flat_dark' => array(
+		'borders' => 'none',
+		'background_color' => '#2f3033',
+		'rounding' => '3px',
+		'texture' => 'none',
+		'title_text_color' => '#e2e2e2',
+		'subtitle_text_color' => '#b9b9b9',
+		'title_font_weight' => '400',
+		'subtitle_font_weight' => '400',
+		'shadow' => 0,
+	),
+);
