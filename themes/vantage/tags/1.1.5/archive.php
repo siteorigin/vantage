ccca7d8958a6abc286239c4135b7a60c88753a3a
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-11-28 14:33:43 +0000

    Tagged Vantage

---
+++ themes/vantage/tags/1.1.5/archive.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package vantage
+ * @since vantage 1.0
+ * @license GPL 2.0
+ */
+
+get_header(); ?>
+
+<section id="primary" class="content-area">
+	<div id="content" class="site-content" role="main">
+
+		<header class="page-header">
+			<h1 id="page-title"><?php echo vantage_get_archive_title() ?></h1>
+			<?php
+			if ( is_category() ) {
+				// show an optional category description
+				$category_description = category_description();
+				if ( ! empty( $category_description ) )
+					echo apply_filters( 'vantage_category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
+
+			}
+			elseif ( is_tag() ) {
+				// show an optional tag description
+				$tag_description = tag_description();
+				if ( ! empty( $tag_description ) )
+					echo apply_filters( 'vantage_tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
+			}
+			?>
+		</header><!-- .page-header -->
+
+		<?php get_template_part( 'loops/loop', siteorigin_setting('blog_archive_layout') ) ?>
+
+	</div><!-- #content .site-content -->
+</section><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ themes/vantage/tags/1.1.5/slider/demo.php
@@ -0,0 +1,20 @@
+<div class="flexslider" id="metaslider-demo">
+	<ul class="slides">
+
+		<li>
+			<div class="content">
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-responsive-overlay.png" width="1080" height="420" alt="<?php esc_attr_e('Demo Slide', 'vantage') ?>" />
+			</div>
+
+			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-4.jpg" class="msDefaultImage" width="1080" height="420" alt="<?php esc_attr_e('Demo Slide', 'vantage') ?>" />
+		</li>
+
+		<li>
+			<div class="content">
+				<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-plugins-overlay.png" width="1080" height="420" alt="<?php esc_attr_e('Demo Slide', 'vantage') ?>" />
+			</div>
+			<img src="<?php echo get_template_directory_uri() ?>/slider/backgrounds/slide-1.jpg" class="msDefaultImage" width="1080" height="420" alt="<?php esc_attr_e('Demo Slide', 'vantage') ?>" />
+		</li>
+
+	</ul>
+</div>
