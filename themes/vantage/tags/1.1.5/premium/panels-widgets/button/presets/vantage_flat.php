0060520bf0ee1e4dfeae9a5d348b3064aa39ec72
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-11-28 13:51:17 +0000

    Tagged Vantage

---
+++ themes/vantage/tags/1.1.5/premium/panels-widgets/button/presets/vantage_flat.php
@@ -0,0 +1,44 @@
+<?php
+
+return array(
+	'red' => array(
+		'background_color' => '#db0019',
+	),
+	'crayola_red' => array(
+		'background_color' => '#ea004a',
+	),
+	'orange' => array(
+		'background_color' => '#f65800',
+	),
+	'gold' => array(
+		'background_color' => '#ed9f00',
+	),
+	'pink' => array(
+		'background_color' => '#ff73a9',
+	),
+	'green' => array(
+		'background_color' => '#69c723',
+	),
+	'dark_green' => array(
+		'background_color' => '#00b450',
+	),
+	'light_blue' => array(
+		'background_color' => '#00c5ea',
+	),
+	'dark_blue' => array(
+		'background_color' => '#0677c0',
+	),
+	'violet' => array(
+		'background_color' => '#7113b1',
+	),
+	'black' => array(
+		'background_color' => '#2f3033',
+	),
+	'white' => array(
+		'background_color' => '#efefef',
+		'text_lighten' => '-55%',
+	),
+	'grey' => array(
+		'background_color' => '#9b9c9d',
+	),
+);
---
+++ themes/vantage/tags/1.1.5/search.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package vantage
+ * @since vantage 1.0
+ * @license GPL 2.0
+ */
+
+get_header(); ?>
+
+<section id="primary" class="content-area">
+	<div id="content" class="site-content" role="main">
+
+	<?php if ( have_posts() ) : ?>
+
+		<header class="page-header">
+			<h1 id="page-title"><?php printf( __( 'Search Results for: %s', 'vantage' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+		</header><!-- .page-header -->
+
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content' ); ?>
+
+		<?php endwhile; ?>
+
+		<?php vantage_content_nav( 'nav-below' ); ?>
+
+	<?php else : ?>
+
+		<?php get_template_part( 'no-results', 'search' ); ?>
+
+	<?php endif; ?>
+
+	</div><!-- #content .site-content -->
+</section><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
