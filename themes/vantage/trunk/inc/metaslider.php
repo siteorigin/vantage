e12b9fae6ee396de2b80f66bb69f7a4e77cdf264
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-01-13 09:41:43 +0000

    * Fixed circle icon widget issue. * Meta Slider compatibility fix. * Improved WooCommerce responsive.

--- themes/vantage/trunk/inc/metaslider.php
+++ themes/vantage/trunk/inc/metaslider.php
@@ -8,7 +8,7 @@
  * @return string
  */
 function vantage_metaslider_themes($themes, $current){
-	$themes .= "<option value='vantage' class='option flex' ".selected('vantage', $current).">".__('Vantage (Flex)', 'vantage')."</option>";
+	$themes .= "<option value='vantage' class='option flex' ".selected('vantage', $current, false).">".__('Vantage (Flex)', 'vantage')."</option>";
 	return $themes;
 }
 add_filter('metaslider_get_available_themes', 'vantage_metaslider_themes', 5, 2);
