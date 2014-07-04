3ec7699c99436bedb9789e1e2162e6c281f15c17
Greg Priday <greg@siteorigin.com>
2014-07-04 19:23:57 +0000

    Made sure everything is compatible with new Page Builder lite.

--- inc/panels.php
+++ inc/panels.php
@@ -173,21 +173,6 @@ function vantage_prebuilt_page_layouts($layouts){
 add_filter('siteorigin_panels_prebuilt_layouts', 'vantage_prebuilt_page_layouts');
 
 /**
- * Configure the SiteOrigin page builder settings.
- * 
- * @param $settings
- * @return mixed
- */
-function vantage_panels_settings($settings){
-	$settings['home-page'] = true;
-	$settings['margin-bottom'] = 35;
-	$settings['home-page-default'] = 'default-home';
-	$settings['responsive'] = siteorigin_setting( 'layout_responsive' );
-	return $settings;
-}
-add_filter('siteorigin_panels_settings', 'vantage_panels_settings');
-
-/**
  * Add row styles.
  *
  * @param $styles
