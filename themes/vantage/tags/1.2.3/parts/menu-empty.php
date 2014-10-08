e6a5c92f28f545dc24c6527d48d3712944f9ad07
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-10-08 09:05:56 +0000

    Tagged Vantage

--- themes/vantage/tags/1.2.3/parts/menu-empty.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Part Name: Empty Menu
- */
-?>
-<nav role="navigation" class="site-navigation main-navigation primary">
-	<div class="full-container">
-	</div>
-</nav><!-- .site-navigation .main-navigation -->
