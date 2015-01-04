9ad6a2a30146be79cbab761ed80f8631c5e38c78
gpriday <gpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2015-01-04 16:04:37 +0000

    Added HTTPS to all links.

--- themes/vantage/trunk/inc/settings.php
+++ themes/vantage/trunk/inc/settings.php
@@ -116,7 +116,7 @@ function vantage_theme_settings(){
 			__('This theme supports <a href="%s" target="_blank">Meta Slider</a>. <a href="%s">Install it</a> for free to create beautiful responsive sliders - <a href="%s" target="_blank">More Info</a>', 'vantage'),
 			'http://sorig.in/metaslider',
 			siteorigin_metaslider_install_link(),
-			'http://siteorigin.com/vantage-documentation/slider/'
+			'https://siteorigin.com/vantage-documentation/slider/'
 		)
 	));
 
--- themes/vantage/trunk/parts/footer.php
+++ themes/vantage/trunk/parts/footer.php
@@ -15,6 +15,6 @@
 		</div><!-- #site-info -->
 	<?php endif; ?>
 
-	<?php echo apply_filters( 'vantage_footer_attribution', '<div id="theme-attribution">' . sprintf( __('A <a href="%s">SiteOrigin</a> Theme', 'vantage'), 'http://siteorigin.com') . '</div>' ) ?>
+	<?php echo apply_filters( 'vantage_footer_attribution', '<div id="theme-attribution">' . sprintf( __('A <a href="%s">SiteOrigin</a> Theme', 'vantage'), 'https://siteorigin.com') . '</div>' ) ?>
 
 </footer><!-- #colophon .site-footer -->
