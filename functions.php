e2b3886a1a6ef392e1256c024b851df4b931e45f
Braam Genis <braamgenis@gmail.com>
2015-01-07 15:41:41 +0200

    212 Add a field for Google Webmaster Tools "Updated" requirement. - removed duplicate vantage_posted_on function from functions.php and updated correct one in inc/template-tags.php.

--- functions.php
+++ functions.php
@@ -386,30 +386,3 @@ function vantage_responsive_header(){
 	}
 }
 add_action('wp_head', 'vantage_responsive_header');
-
-if ( ! function_exists( 'vantage_posted_on' ) ) :
-	/**
-	 * Prints HTML with meta information for the current post-date/time and author.
-	 *
-	 * @since vantage 1.0
-	 */
-	function vantage_posted_on() {
-		$posted_on_parts = array(
-			'on' => __('Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><time class="updated" datetime="' . esc_attr( get_the_modified_date( 'c' ) ) . '">' . esc_html( get_the_modified_date() ) . '</time></span>', 'vantage'),
-			'by' => __( '<span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'vantage' ),
-		);
-		$posted_on_parts = apply_filters('vantage_post_on_parts', $posted_on_parts);
-
-
-		$posted_on = sprintf( implode(' ', $posted_on_parts),
-			esc_url( get_permalink() ),
-			esc_attr( get_the_time() ),
-			esc_attr( get_the_date( 'c' ) ),
-			apply_filters('vantage_post_on_date', esc_html( get_the_date() )),
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			esc_attr( sprintf( __( 'View all posts by %s', 'vantage' ), get_the_author() ) ),
-			get_the_author()
-		);
-		echo apply_filters('vantage_posted_on', $posted_on);
-	}
-endif;
