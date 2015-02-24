de8b5d0bad278e3d7f0b6b58fd69bf0d9563c1bf
gregpriday <greg@siteorigin.com>
2015-02-24 17:30:06 +0200

    Removed URL shortners
    Removed all URL shortners and redirects.

--- inc/settings.php
+++ inc/settings.php
@@ -122,7 +122,7 @@ function vantage_theme_settings(){
 		'options' => siteorigin_metaslider_get_options(true),
 		'description' => sprintf(
 			__('This theme supports <a href="%s" target="_blank">Meta Slider</a>. <a href="%s">Install it</a> for free to create beautiful responsive sliders - <a href="%s" target="_blank">More Info</a>', 'vantage'),
-			'http://sorig.in/metaslider',
+			'https://siteorigin.com/metaslider/',
 			siteorigin_metaslider_install_link(),
 			'https://siteorigin.com/vantage-documentation/slider/'
 		)
