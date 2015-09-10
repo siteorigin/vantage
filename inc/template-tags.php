ee236af010f1e7b055cec0f66b1a04c386f220a1
Braam Genis <braamgenis@gmail.com>
2015-09-10 14:02:22 +0200

    Extracted HTML from 'Posted on' string to be translated.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -146,24 +146,30 @@ if ( ! function_exists( 'vantage_posted_on' ) ) :
  * @since vantage 1.0
  */
 function vantage_posted_on() {
-	$posted_on_parts = array(
-		'on' => __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><time class="updated" datetime="%5$s">%6$s</time>', 'vantage'),
-		'by' => __( '<span class="byline"> by <span class="author vcard"><a class="url fn n" href="%7$s" title="%8$s" rel="author">%9$s</a></span></span>', 'vantage' ),
-	);
-	$posted_on_parts = apply_filters('vantage_post_on_parts', $posted_on_parts);
-
-
-	$posted_on = sprintf( implode(' ', $posted_on_parts),
+	$date_time = '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><time class="updated" datetime="%5$s">%6$s</time>';
+	$date_time = sprintf( $date_time,
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),
 		apply_filters('vantage_post_on_date', esc_html( get_the_date() )),
 		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$author = sprintf('<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
 		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 		esc_attr( sprintf( __( 'View all posts by %s', 'vantage' ), get_the_author() ) ),
 		get_the_author()
 	);
+
+	$posted_on_parts = array(
+		'on' => __( 'Posted on %1$s', 'vantage'),
+		'by' => __( '<span class="byline"> by %2$s</span>', 'vantage' ),
+	);
+	$posted_on_parts = apply_filters('vantage_post_on_parts', $posted_on_parts);
+
+
+	$posted_on = sprintf( implode(' ', $posted_on_parts), $date_time, $author );
 	echo apply_filters('vantage_posted_on', $posted_on);
 }
 endif;
