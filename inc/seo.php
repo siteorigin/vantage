4b2d6439bbb54a337a1ebf22cdd5c3214baa4877
gregpriday <greg@siteorigin.com>
2016-02-24 13:42:51 +0200

    Typo correction

--- inc/seo.php
+++ inc/seo.php
@@ -1,8 +1,8 @@
 <?php
 
-function vantage_display_yaost_seo_breadcrumbs() {
+function vantage_display_yoast_seo_breadcrumbs() {
 	if ( function_exists('yoast_breadcrumb') && !is_front_page() && siteorigin_setting('navigation_yoast_breadcrumbs') ) {
 		yoast_breadcrumb('<div id="yoast-breadcrumbs"><div class="full-container">','</div></div>');
 	}
 }
-add_filter('vantage_main_top', 'vantage_display_yaost_seo_breadcrumbs');
+add_filter('vantage_main_top', 'vantage_display_yoast_seo_breadcrumbs');
