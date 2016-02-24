b6fd1bdb61e820b8dc31bc20a5736f3abbcdd2ec
gregpriday <greg@siteorigin.com>
2016-02-24 12:59:17 +0200

    Removed old retina code

--- js/jquery.theme-main.js
+++ js/jquery.theme-main.js
@@ -350,24 +350,6 @@ jQuery(function($){
         });
     });
 
-    // Substitute any retina images
-    var pixelRatio = !!window.devicePixelRatio ? window.devicePixelRatio : 1;
-    if( pixelRatio > 1 ) {
-        $('img[data-retina-image]').each( function(){
-            var $$ = $(this);
-            $$.attr('src', $$.data('retina-image'));
-
-            // If the width attribute isn't set, then lets scale to 50%
-            if( typeof $$.attr('width') === 'undefined' ) {
-                $$.load( function(){
-                    var size = [$$.width(), $$.height()];
-                    $$.width(size[0]/2);
-                    $$.height(size[1]/2);
-                } );
-            }
-        } );
-    }
-
     // Resize the header widget area
     $('#header-sidebar').each(function(){
         var $$ = $(this);
