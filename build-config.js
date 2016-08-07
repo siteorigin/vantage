84b99623f2f963dcc860e7923c011b908990685b
gregpriday <greg@siteorigin.com>
2016-08-07 18:10:18 +0200

    Added premium folder JS to build config

--- build-config.js
+++ build-config.js
@@ -50,6 +50,7 @@ module.exports = {
     js: {
         src: [
             'js/**/*.js',
+            'premium/**/*.js',
             'inc/*/js/**/*.js',
             '!{node_modules,node_modules/**}',  // Ignore node_modules/ and contents
             '!{tests,tests/**}',                // Ignore tests/ and contents
