2dc6f91c8b2e23e2d35f1e674989488decc548db
Andrew Misplon <amisplon@gmail.com>
2017-06-29 15:08:24 +0200

    Update constants

--- functions.php
+++ functions.php
@@ -7,8 +7,9 @@
  * @license GPL 2.0
  */
 
-define('SITEORIGIN_THEME_VERSION', 'dev');
-define('SITEORIGIN_THEME_JS_PREFIX', '');
+define( 'SITEORIGIN_THEME_VERSION', 'dev' );
+define( 'SITEORIGIN_THEME_JS_PREFIX', '' );
+define( 'SITEORIGIN_THEME_CSS_PREFIX', '' );
 
 // Load the new settings framework
 include get_template_directory() . '/inc/settings/settings.php';
