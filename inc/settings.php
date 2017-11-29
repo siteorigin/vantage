9077f18789dfd05643d82bf1b6ee48d7bdaf1ca8
adiraomj <adiraomj@outlook.com>
2017-11-29 11:10:35 +0530

    Updated slider options for customizer

--- inc/settings.php
+++ inc/settings.php
@@ -174,9 +174,9 @@ function vantage_theme_settings(){
 	$settings->add_field('home', 'slider', 'select', __('Home Page Slider', 'vantage'), array(
 		'options' => siteorigin_metaslider_get_options(true),
 		'description' => sprintf(
-			__('This theme supports <a href="%s" target="_blank">Meta Slider</a>. <a href="%s">Install it</a> for free to create beautiful responsive sliders - <a href="%s" target="_blank">More Info</a>', 'vantage'),
-			'https://siteorigin.com/metaslider/',
-			siteorigin_metaslider_install_link(),
+			__('This theme supports <a href="%s" target="_blank">Smart Slider 3</a>. <a href="%s">Install it</a> for free to create beautiful responsive sliders - <a href="%s" target="_blank">More Info</a>', 'vantage'),
+			'https://siteorigin.com/smart-slider-3/',
+			siteorigin_smartslider_install_link(),
 			'https://siteorigin.com/vantage-documentation/home-page-slider/'
 		)
 	));
