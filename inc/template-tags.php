5f49d8a7ce2cbbff09a2d6a8cc07a4f3041e5bfb
Andrew Misplon <amisplon@gmail.com>
2019-02-06 12:38:41 +0200

    PHP spacing fix

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -52,8 +52,8 @@ function vantage_content_nav( $nav_id ) {
 	$jetpack_infinite_scroll_active = class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'infinite-scroll' );
 	// Check if we're in the Page Builder Post Loop widget.
 	$is_page_builder_post_loop_widget = class_exists( 'SiteOrigin_Panels_Widgets_PostLoop' ) &&
-										method_exists( 'SiteOrigin_Panels_Widgets_PostLoop', 'is_rendering_loop' ) &&
-										SiteOrigin_Panels_Widgets_PostLoop::is_rendering_loop();
+	                                    method_exists( 'SiteOrigin_Panels_Widgets_PostLoop', 'is_rendering_loop' ) &&
+	                                    SiteOrigin_Panels_Widgets_PostLoop::is_rendering_loop();
 
 	if ( $jetpack_infinite_scroll_active && ! $is_page_builder_post_loop_widget ) {
 		return;
