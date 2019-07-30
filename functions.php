0b8da97011100240bb62cfd350f3e55b0d2fc26c
Andrew Misplon <amisplon@gmail.com>
2019-07-30 11:15:24 +0200

    Deleted the theme tour

--- functions.php
+++ functions.php
@@ -29,11 +29,7 @@ include get_template_directory() . '/inc/menu.php';
 include get_template_directory() . '/inc/breadcrumbs.php';
 include get_template_directory() . '/inc/customizer.php';
 include get_template_directory() . '/inc/legacy.php';
-
-// include get_template_directory() . '/tour/tour.php';
-
 include get_template_directory() . '/fontawesome/icon-migration.php';
-
 
 if ( ! function_exists( 'vantage_setup' ) ) :
 /**
