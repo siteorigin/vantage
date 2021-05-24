fda88768841f0ceea079ad01cfbd10825256c25f
Andrew Misplon <amisplon@gmail.com>
2021-05-24 16:38:51 +0200

    Spaces to tabs

--- inc/customizer/js/admin.js
+++ inc/customizer/js/admin.js
@@ -2,24 +2,24 @@
 /* globals jQuery, wp, soCustomizeAdmin, confirm */
 
 wp.customize.bind( 'ready', function( value ){
-    var $ = jQuery;
+	var $ = jQuery;
 
-    var button = null;
-    $('#accordion-panel-siteorigin_theme_settings ').one('expanded', function(){
-        var buttonWrapper = $("<div id='siteorigin-customizer-reset'></div>")
-            .appendTo( $('#accordion-panel-siteorigin_theme_settings .accordion-section-content.description') )
-            .append(
-                $("<a class='reset-customizations button-secondary'></a>")
-                    .html( soCustomizeAdmin.button )
-                    .attr( 'href', soCustomizeAdmin.action )
-            );
+	var button = null;
+	$('#accordion-panel-siteorigin_theme_settings ').one('expanded', function(){
+		var buttonWrapper = $("<div id='siteorigin-customizer-reset'></div>")
+			.appendTo( $('#accordion-panel-siteorigin_theme_settings .accordion-section-content.description') )
+			.append(
+				$("<a class='reset-customizations button-secondary'></a>")
+					.html( soCustomizeAdmin.button )
+					.attr( 'href', soCustomizeAdmin.action )
+			);
 
-        buttonWrapper.find( 'a.reset-customizations' ).on( 'click', function( e ) {
-            if( !confirm( soCustomizeAdmin.confirm ) ) {
-                e.preventDefault();
-            }
-        });
-    });
+		buttonWrapper.find( 'a.reset-customizations' ).on( 'click', function( e ) {
+			if( !confirm( soCustomizeAdmin.confirm ) ) {
+				e.preventDefault();
+			}
+		});
+	});
 	
 	$( '#accordion-panel-theme_settings' ).one( 'expanded', function () {
 		var toggleSliderStretch = function ( selectedSlider ) {
